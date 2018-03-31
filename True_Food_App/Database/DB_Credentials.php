ef912c560296fde76ff0b55196ad77b46e7b3a2a
Erick Javier Duarte <EliteAccount@tests-MacBook.local>
2018-03-30 23:14:04 -0600

    Files for Xampp, dump FOLDER in htdocs fodler from Xammp

---
+++ True_Food_App/Database/DB_Credentials.php
@@ -0,0 +1,14 @@
+<?php
+
+// Created by Erick Duarte
+// 1/28/2018
+// User: True Food
+
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'TrueFood');
+ 
+define('USER_CREATED', 0);
+define('USER_ALREADY_EXIST', 1);
+define('USER_NOT_CREATED', 2);
