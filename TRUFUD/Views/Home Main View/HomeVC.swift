b2203c0e28366831212f9d836655813fe4d5ddae
Erick Javier Duarte <EliteAccount@tests-MacBook.local>
2018-04-02 21:35:07 -0600

    wired home vc and recipes vc

---
+++ TRUFUD/Views/Home Main View/HomeVC.swift
@@ -0,0 +1,55 @@
+//
+//  HomeVC.swift
+//  TRUFUD
+//
+//  Created by Erick Javier Duarte on 4/2/18.
+//  Copyright © 2018 Harleaux Carrera. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    @IBAction func shop_vc(_ sender: Any) {
+    }
+  
+    @IBAction func recipes_vc(_ sender: Any) {
+
+    }
+    @IBAction func farms_vc(_ sender: Any) {
+    }
+    
+    @IBAction func shopingCart_vc(_ sender: Any) {
+    }
+    
+    @IBAction func news_vc(_ sender: Any) {
+    }
+    
+    @IBAction func calendar_vc(_ sender: Any) {
+    }
+    
+    @IBAction func account_vc(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
